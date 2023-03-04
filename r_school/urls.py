b98aaec9ebc5b3bc200749a33de470d056e23089
greatraksin <greatraksin@icloud.com>
2023-03-04 14:21:27 +0300

    Adding login/register urls

--- r_school/urls.py
+++ r_school/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from accounts.views import login_view, logout_view, register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+
     path('', include('school.urls'), name='school')
 ]
 
